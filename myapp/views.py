ae30d184e0b5d398bcacf1d9c08e48a678d0e66a
abhisheksaxena1998 <abhi123923@gmail.com>
2020-01-21 19:46:13 +0530

    fixed bugs

--- myapp/views.py
+++ myapp/views.py
@@ -279,124 +279,189 @@ def result(request):
 
 def api(request):
     try:
-        text = request.GET['query']
-        aburl = -1
-        digits = '0123456789'
-        if text[8] in digits:
-            oneval = -1
-        else:
-            oneval = 1
-        if len(text) > 170:
-            secval = -1
-        else:
-            secval = 1
-        if '@' in text:
-            thirdval = -1
-        else:
-            thirdval = 1
-        k = text.count('//')
-        if k > 1:
-            fourthval = -1
-        else:
-            fourthval = 1
+        text=request.GET['query']
+        
+        if text.startswith('https://') or text.startswith('http://'):
 
-        if '-' in text:
-            fifthval = -1
-        else:
-            fifthval = 1
-        if 'https' in text:
-            sixthval = 1
-        else:
-            sixthval = -1
-        temp = text
-        temp = temp[6:]
-        k1 = temp.count('https')
-
-        if k1 >= 1:
-            seventhval = -1
-        else:
-            seventhval = 1
-        if 'about:blank' in text:
-            eighthval = -1
-        else:
-            eighthval = 1
-        if 'mail()' or 'mailto:' in text:
-            ninthval = -1
-        else:
-            ninthval = 1
-        re = text.count('//')
-        if re > 3:
-            tenthval = -1
-        else:
-            tenthval = 1
-
-        import whois
-        from datetime import datetime
-
-        url = text
-
-        try:
-            res = whois.whois(url)
-            try:
-                a = res['creation_date'][0]
-                b = datetime.now()
-                c = b - a
-                d = c.days
-            except:
-                a = res['creation_date']
-                b = datetime.now()
-                c = b - a
-                d = c.days
-            if d > 365:
-                eleventhval = 1
+            if len(text)<=9:
+                return render(request,'errorpage.html')
+            aburl=-1
+            digits="0123456789"
+            if text[8] in digits:
+                oneval=-1
             else:
-                eleventhval = -1
-        except:
-            aburl = 1
-            eleventhval = -1
+                oneval=1    
+            if len(text)>170:
+                secval=-1
+            else:
+                secval=1  
+            if "@" in text:
+                thirdval=-1
+            else:
+                thirdval=1    
+            k=text.count("//")          
+            if k>1:
+                fourthval=-1
+            else:
+                fourthval=1
+                
+            if "-" in text:
+                fifthval=-1
+            else:
+                fifthval=1         
+            if "https" in text:
+                sixthval=1
+            else:
+                sixthval=-1
+            temp=text
+            temp=temp[6:]
+            k1=temp.count("https")
 
-        if aburl == 1:
-            twelthval = -1
-        else:
-            twelthval = 1
+            if k1 >=1:
+                seventhval=-1
+            else:
+                seventhval=1
+            if "about:blank" in text:
+                eighthval=-1
+            else:
+                eighthval=1
+            if "mail()" or "mailto:" in text:
+                ninthval=-1
+            else:
+                ninthval=1
+            re=text.count("//")          
+            if re>3:
+                tenthval=-1
+            else:
+                tenthval=1    
 
-        filename = 'phish_trainedv0.sav'
+            import whois
+            from datetime import datetime
 
-        loaded_model = joblib.load(filename)
+            url=text
 
-        arg = loaded_model.predict([[
-            oneval,
-            secval,
-            thirdval,
-            fourthval,
-            fifthval,
-            sixthval,
-            seventhval,
-            eighthval,
-            ninthval,
-            tenthval,
-            eleventhval,
-            twelthval,
-            ]])
-        #print arg[0]
-        if arg[0] == 1:
-            te = 'Legitimate'
-        else:
-            te = 'Malicious'
-        from json.encoder import JSONEncoder
-        final_entity = {'predicted_argument': [int(arg[0])]}
+            try:
+                res=whois.whois(url)
+                try:
+                    a=res['creation_date'][0]
+                    b=datetime.now()
+                    c=b-a
+                    d=c.days
+                except:
+                    a=res['creation_date']
+                    b=datetime.now()
+                    c=b-a
+                    d=c.days
+                if d>365:
+                    eleventhval=1
+                else:
+                    eleventhval=-1
+            except:
+                aburl=-1
+                eleventhval=-1   
 
-        # directly called encode method of JSON
+            if aburl==-1:
+                twelthval=-1
+            else:
+                twelthval=1 
+            import urllib.request, sys, re
+            import xmltodict, json
 
-        #print JSONEncoder().encode(final_entity)
-        if arg[0] == 1:
-            mal = False
-        else:
-            mal = True
-        obj = Url()
-        obj.link = text
-        obj.result = te 
-        obj.save()
+            try:
+                xml = urllib.request.urlopen('http://data.alexa.com/data?cli=10&dat=s&url={}'.format(text)).read()
+
+                result= xmltodict.parse(xml)
+
+                data = json.dumps(result).replace("@","")
+                data_tojson = json.loads(data)
+                url = data_tojson["ALEXA"]["SD"][1]["POPULARITY"]["URL"]
+                rank= int(data_tojson["ALEXA"]["SD"][1]["POPULARITY"]["TEXT"])
+                print ("rank",rank)
+                if rank<=100000:
+                    thirt=1
+                else:
+                    thirt=-1
+                print (thirt)    
+            except:
+                thirt=-1 
+                rank="Not Indexed by Alexa"
+                print (rank)                  
+
+
+
+
+            filename = 'phish_trainedv3.sav'
+
+            loaded_model = joblib.load(filename)
+
+            arg=loaded_model.predict(([[oneval,secval,thirdval,fourthval,fifthval,seventhval,eighthval,ninthval,tenthval,eleventhval,twelthval,thirt]]))
+            print (arg[0])
+            import whois
+            url=text
+            
+            #print (res)
+            try:
+                res=whois.whois(url)
+                name=res["name"]
+                print (res["name"])
+                org=res['org']
+                print (res['org'])
+                add=res['address']
+                print (res['address'])
+                city=res['city']
+                print (res['city'])
+                state=res['state']
+                print (res['state'])
+                ziip=res['zipcode']
+                print (res['zipcode'])
+                country=res['country']
+                print (res['country'])
+                emails=res["emails"][0]   
+                print (res["emails"][0])
+                dom=res['domain_name']
+                print (res['domain_name'])                
+            except:
+                name="Not Found"
+                org="Not Found"
+                add="Not Found"
+                city="Not Found"
+                state="Not Found"
+                ziip="Not Found"
+                country="Not Found"
+                emails="Not Found"   
+                dom="Not Found"
+
+            if dom=="Not Found" and rank=="Not Indexed by Alexa" :
+                arg[0]=-1
+                #phishing
+
+            if arg[0]==1:
+                te="Legitimate"
+            else:
+                te="Malicious"  
+            if arg[0] == 1:
+                mal = True
+            else:
+                mal = False      
+            if arg[0] == 1:
+                malstatus = False
+            else:
+                malstatus = True                 
+            from json.encoder import JSONEncoder
+            final_entity = { "predicted_argument": [int(arg[0])]}
+            # directly called encode method of JSON
+            print (JSONEncoder().encode(final_entity)) 
+            
+            print (dom,rank)
+                     
+            tags = [name,org,state,add,city,ziip,country,emails,dom,rank]
+
+            tags = list(filter(lambda x: x!="Not Found",tags))
+            tags.append(text)
+            obj = Url()
+            obj.link = text
+            obj.result = te 
+            obj.save()
 
         '''return render(request, 'result.html',
                   {'result': 'Real-time analysis successfull',
@@ -405,15 +470,10 @@ def api(request):
         import datetime
         mydict = {
             "query" : url,
-            "malware" : mal,
+            "malware" : malstatus,
             "datetime" : str(datetime.datetime.now())
         }
-        response =  JsonResponse(mydict)
-        response["Access-Control-Allow-Origin"] = "*"
-        response["Access-Control-Allow-Methods"] = "GET, OPTIONS"
-        response["Access-Control-Max-Age"] = "1000"
-        response["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
-        return response
+        return JsonResponse(mydict)
     except:
         return render(request,'404.html')                      
 
